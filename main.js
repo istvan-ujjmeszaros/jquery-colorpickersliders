0b411d4594bad9a0f78fc5b72089e0e728cab85b
imaguiraga <imaguiraga@gmail.com>
2013-11-13 04:12:01 -0500

    cleanup

--- main.js
+++
@@ -1,7 +0,0 @@
- $("#destroy").on("click",function(){
-	$(".a-popup").trigger("colorpickersliders.destroy");
- });
- 
-  $("#reset").on("click",function(){
-	$(".a-popup").trigger("colorpickersliders.reset");
- });
